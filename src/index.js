7da9b4b11d337b850e7a1f2ffae6e8f80cd21526
Rustam-Ulaevich <praporglamur@gmail.com>
2024-07-17 15:01:03 +0500

    add reducer

--- src/index.js
+++ src/index.js
@@ -3,6 +3,27 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux';
+
+const defaultState = {
+  cash: 5
+}
+
+const reducer = (state = defaultState, action) => {
+  switch(action.type){
+    case 'ADD-CASH':
+      return {...state, cash: state.cash + action.payload}
+
+    case 'GET-CASH':
+      return {...state, cash: state.cash - action.payload}
+    
+    default: return state
+  }
+}
+
+const store = createStore(reducer)
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -11,7 +32,4 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
